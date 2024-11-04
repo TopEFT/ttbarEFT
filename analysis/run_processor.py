5cbaac4a82c4f68e96c4701292f97e66ae8ff05f
Chrstopher McGrady <cmcgrad2@nd.edu>
2024-11-04 10:55:50 -0500

    proc options added

--- analysis/run_processor.py
+++ analysis/run_processor.py
@@ -5,6 +5,7 @@ import time
 import cloudpickle
 import gzip
 import os
+import cProfile
 import importlib
 
 import numpy as np
@@ -14,65 +15,67 @@ from coffea.nanoevents import NanoAODSchema
 from topcoffea.modules import utils
 import topcoffea.modules.remote_environment as remote_environment
 
-LST_OF_KNOWN_EXECUTORS = ["futures","work_queue"]
+LST_OF_KNOWN_EXECUTORS = ["futures", "work_queue"]
+proc_options = ["net_disc", "kinematics_processor", "weight_processor", "analysis_processor"]
 
-if __name__ == '__main__':
-
-    import argparse
+def main():
     parser = argparse.ArgumentParser(description='You can customize your run')
     parser.add_argument('jsonFiles'        , nargs='?', help = 'Json file(s) containing files and metadata')
     parser.add_argument('--executor','-x'  , default='work_queue', help = 'Which executor to use')
     parser.add_argument('--prefix', '-r'   , nargs='?', default='', help = 'Prefix or redirector to look for the files')
-    parser.add_argument('--pretend'        , action='store_true', help = 'Read json files but, not execute the analysis')
     parser.add_argument('--nworkers','-n' , default=8  , help = 'Number of workers')
     parser.add_argument('--chunksize','-s', default=100000  , help = 'Number of events per chunk')
     parser.add_argument('--nchunks','-c'  , default=None  , help = 'You can choose to run only a number of chunks')
     parser.add_argument('--outname','-o'  , default='histos', help = 'Name of the output file with histograms')
     parser.add_argument('--treename'      , default='Events', help = 'Name of the tree inside the files')
-    parser.add_argument('--wc-list', action='extend', nargs='+', help = 'Specify a list of Wilson coefficients to use in filling histograms.')
     parser.add_argument('--hist-list', action='extend', nargs='+', help = 'Specify a list of histograms to fill.')
     parser.add_argument('--port', default='9123-9130', help = 'Specify the Work Queue port. An integer PORT or an integer range PORT_MIN-PORT_MAX.')
-    parser.add_argument('--processor', '-p', default='analysis_processor.py', help='Specify processor file name')
+    parser.add_argument('--processor', '-p', default='kinematics_processor', help='Specify processor name (without .py)')
+    parser.add_argument('--lnet',  '-l', default='ctq8_5.0', help='Specify linear networks as array of strings (i.e. ["ctq8_5.0", "ctq1_1.0"])')
 
     args        = parser.parse_args()
-    jsonFiles  = args.jsonFiles
+    jsonFiles   = args.jsonFiles
     executor    = args.executor
     prefix      = args.prefix
-    pretend     = args.pretend
     nworkers    = int(args.nworkers)
     chunksize   = int(args.chunksize)
     nchunks     = int(args.nchunks) if not args.nchunks is None else args.nchunks
     outname     = args.outname
     treename    = args.treename
-    wc_lst = args.wc_list if args.wc_list is not None else []
-    proc_file   = args.processor
-    proc_name   = args.processor[:-3]
+    proc        = args.processor
+    lnet        = args.lnet
 
+    if proc not in proc_options:
+        raise Exception(f"The \"{proc}\" processor is not known. Modify the run script or choose from the list of known processors: {proc_options}. Exiting. ")
+
+    proc_file = proc+'.py'
     print("\n running with processor: ", proc_file, '\n')
 
-    analysis_processor = importlib.import_module(proc_name)
-
-    # Check if we have valid options
     if executor not in LST_OF_KNOWN_EXECUTORS:
         raise Exception(f"The \"{executor}\" executor is not known. Please specify an executor from the known executors ({LST_OF_KNOWN_EXECUTORS}). Exiting.")
 
     if executor == "work_queue":
-        # construct wq port range
         port = list(map(int, args.port.split('-')))
         if len(port) < 1:
             raise ValueError("At least one port value should be specified.")
         if len(port) > 2:
             raise ValueError("More than one port range was specified.")
         if len(port) == 1:
-            # convert single values into a range of one element
             port.append(port[0])
 
-    if args.hist_list == ["kinematics"]:
-        hist_lst = ["tops_pt", "avg_top_pt", "l0pt", "dr_leps", "ht", "jets_pt", "j0pt", "njets", "mtt", "mll"]
+    if args.hist_list == ["kinematicsDNN"]:
+        hist_lst = ['Lep1_pt', 'Lep2_pt', 'Lep1_eta',
+                    'Lep2_eta', 'Lep1_phi', 'Lep2_phi', 
+                    'nJet30', 'jet1_pt', 'jet2_pt']
+    elif args.hist_list == ["net_disc"]:
+        hist_lst = ['net_disc']
+    elif args.hist_list == ["kinematics"]:
+        hist_lst = ["tops_pt", "avg_top_pt", "l0pt", 
+                    "dr_leps", "ht", "jets_pt", 
+                    "j0pt", "njets", "mtt", "mll"]
     else:
         hist_lst = args.hist_list
 
-    # Load samples from json and setup the inputs to the processor
     samplesdict = {}
     allInputFiles = []
 
@@ -95,14 +98,11 @@ if __name__ == '__main__':
         else:
             allInputFiles.append(jsonFile)
 
-    # Read from cfg files
     for f in allInputFiles:
         if not os.path.isfile(f):
             raise Exception(f'[ERROR] Input file {f} not found!')
-        # This input file is a json file, not a cfg
         if f.endswith('.json'):
             LoadJsonToSampleName(f, prefix)
-        # Open cfg files
         else:
             with open(f) as fin:
                 print(' >> Reading json from cfg file...')
@@ -124,119 +124,47 @@ if __name__ == '__main__':
                             prefix = l
                         else:
                             LoadJsonToSampleName(l, prefix)
-                    # else:
-                    #     if not l.endswith('.json'):
-                    #         prefix = l
-                    #     else:
-                    #         LoadJsonToSampleName(l, prefix)
 
     flist = {}
     for sname in samplesdict.keys():
-        #flist[sname] = samplesdict[sname]['files']
         redirector = samplesdict[sname]['redirector']
         flist[sname] = [(redirector+f) for f in samplesdict[sname]['files']]
-
-    # Extract the list of all WCs, as long as we haven't already specified one.
-    if len(wc_lst) == 0:
-        for k in samplesdict.keys():
-            for wc in samplesdict[k]['WCnames']:
-                if wc not in wc_lst:
-                    wc_lst.append(wc)
-
-    if len(wc_lst) > 0:
-        # Yes, why not have the output be in correct English?
-        if len(wc_lst) == 1:
-            wc_print = wc_lst[0]
-        elif len(wc_lst) == 2:
-            wc_print = wc_lst[0] + ' and ' + wc_lst[1]
-        else:
-            wc_print = ', '.join(wc_lst[:-1]) + ', and ' + wc_lst[-1]
-            print('Wilson Coefficients: {}.'.format(wc_print))
-    
-    else:
-        print('No Wilson coefficients specified')
-
-    # Run the processor and get the output
-    processor_instance = analysis_processor.AnalysisProcessor(samplesdict,wc_lst,hist_lst)
+        
+    wc_set = ()
+    for item in samplesdict:
+        for i, file_name in enumerate(samplesdict[item]['files']):
+            wc_lst = utils.get_list_of_wc_names(file_name)
+            if i==0:
+                wc_set = set(wc_lst)
+            else:
+                if set(wc_lst) != wc_set:
+                   raise Exception("ERROR: Not all files have same WC list")
 
     if executor == "work_queue":
         executor_args = {
             'master_name': '{}-workqueue-coffea'.format(os.environ['USER']),
-
-            # find a port to run work queue in this range:
             'port': port,
-
             'debug_log': 'debug.log',
             'transactions_log': 'tr.log',
             'stats_log': 'stats.log',
             'tasks_accum_log': 'tasks.log',
-
-            #'environment_file': 'topEFT-env.tar.gz',
             'environment_file': remote_environment.get_environment(
-                extra_pip_local = {"ttbarEFT": ["ttbarEFT", "setup.py"]},
+                extra_conda=["pytorch=2.3.0", "numpy=1.23.5", "pyyaml=6.0.1"],
+                extra_pip_local = {"EFTmva": ["models", "net.py"], 
+                                  },
             ),
-            # 'extra_input_files': ["nanogen_processor.py"],
             'extra_input_files' : [proc_file],
-
             'retries': 5,
-
-            # use mid-range compression for chunks results. 9 is the default for work
-            # queue in coffea. Valid values are 0 (minimum compression, less memory
-            # usage) to 16 (maximum compression, more memory usage).
             'compression': 9,
-
-            # automatically find an adequate resource allocation for tasks.
-            # tasks are first tried using the maximum resources seen of previously ran
-            # tasks. on resource exhaustion, they are retried with the maximum resource
-            # values, if specified below. if a maximum is not specified, the task waits
-            # forever until a larger worker connects.
             'resource_monitor': True,
             'resources_mode': 'auto',
-
-            # this resource values may be omitted when using
-            # resources_mode: 'auto', but they do make the initial portion
-            # of a workflow run a little bit faster.
-            # Rather than using whole workers in the exploratory mode of
-            # resources_mode: auto, tasks are forever limited to a maximum
-            # of 8GB of mem and disk.
-            #
-            # NOTE: The very first tasks in the exploratory
-            # mode will use the values specified here, so workers need to be at least
-            # this large. If left unspecified, tasks will use whole workers in the
-            # exploratory mode.
-            # 'cores': 1,
-            # 'disk': 8000,   #MB
-            # 'memory': 10000, #MB
-
-            # control the size of accumulation tasks. Results are
-            # accumulated in groups of size chunks_per_accum, keeping at
-            # most chunks_per_accum at the same time in memory per task.
             'chunks_per_accum': 25,
             'chunks_accum_in_mem': 2,
-
-            # terminate workers on which tasks have been running longer than average.
-            # This is useful for temporary conditions on worker nodes where a task will
-            # be finish faster is ran in another worker.
-            # the time limit is computed by multipliying the average runtime of tasks
-            # by the value of 'fast_terminate_workers'.  Since some tasks can be
-            # legitimately slow, no task can trigger the termination of workers twice.
-            #
-            # warning: small values (e.g. close to 1) may cause the workflow to misbehave,
-            # as most tasks will be terminated.
-            #
-            # Less than 1 disables it.
             'fast_terminate_workers': 0,
-
-            # print messages when tasks are submitted, finished, etc.,
-            # together with their resource allocation and usage. If a task
-            # fails, its standard output is also printed, so we can turn
-            # off print_stdout for all tasks.
             'verbose': True,
             'print_stdout': False,
         }
 
-
-    # Run the processor and get the output
     tstart = time.time()
 
     if executor == "futures":
@@ -246,22 +174,40 @@ if __name__ == '__main__':
         executor = processor.WorkQueueExecutor(**executor_args)
         runner = processor.Runner(executor, schema=NanoAODSchema, chunksize=chunksize, maxchunks=nchunks, skipbadfiles=False, xrootdtimeout=180)
 
-    output = runner(flist, treename, processor_instance)
+    if proc == 'kinematics_processor':
+        import kinematics_processor
+        processor_instance = kinematics_processor.AnalysisProcessor(samplesdict, wc_lst, hist_lst)
+    elif proc == 'weight_processor':
+        import weight_processor
+        processor_instance = weight_processor.AnalysisProcessor(samplesdict, wc_lst, hist_lst, lnet=lnet) 
+    elif proc == 'net_disc':
+        import net_disc
+        processor_instance = net_disc.AnalysisProcessor(samplesdict, wc_lst, hist_lst)
+    elif proc == 'analysis_processor':
+        analysis_processor = importlib.import_module(proc_name)
+        processor_instance = analysis_processor.AnalysisProcessor(samplesdict,wc_lst,hist_lst)
 
+    output = runner(flist, treename, processor_instance)
+    
     dt = time.time() - tstart
 
     if executor == "work_queue":
-        print('Processed {} events in {} seconds ({:.2f} evts/sec).'.format(nevts_total,dt,nevts_total/dt))
+        print(f'Processed {nevts_total} events in {dt:.2f} seconds ({nevts_total/dt:.2f} evts/sec).')
     elif executor == "futures":
-        print("Processing time: %1.2f s with %i workers (%.2f s cpu overall)" % (dt, nworkers, dt*nworkers, ))
-    
+        print(f'Processing time: {dt:.2f} with {nworkers} ({dt*nworkers:.2f} cpu overall)')
 
-    # Save the output
-    outpath = "."
-    if not os.path.isdir(outpath): os.system("mkdir -p %s"%outpath)
-    out_pkl_file = os.path.join(outpath,outname+".pkl.gz")
+    if not os.path.isdir(outname): os.system("mkdir -p %s"%outname)
+    out_pkl_file = os.path.join(outname,"histeft.pkl.gz")
     print(f"\nSaving output in {out_pkl_file}...")
     with gzip.open(out_pkl_file, "wb") as fout:
         cloudpickle.dump(output, fout)
+        
+    
     print("Done!")
 
+if __name__ == '__main__':
+    profile = False
+    if profile:
+        cProfile.run('main()', filename='profile.txt')
+    else:
+        main()
